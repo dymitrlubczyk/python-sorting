50fe6747843f9f42efc6fdbbe8ab9629faceec46
Dymitr Lubczyk <dymitrlubczyk@dymitrs-mbp.speedport.ip>
2021-08-28 19:42:07 +0200

    Add new test cases

--- tests/constants.py
+++ tests/constants.py
@@ -7,11 +7,11 @@ from common import generate
 
 testCases = [
     np.array([]),
-    np.array([2, 1, 3]),
+    np.array([1]),
     np.array([1, 1, 1]),
-    np.array([6, 2, 4, 8, 2, 1, 3, 4, 5]),
     np.array([1, 2, 3, 4, 5]),
     np.array([5, 4, 3, 2, 1]),
+    generate(10),
     generate(100),
     generate(1000),
     generate(10000)
